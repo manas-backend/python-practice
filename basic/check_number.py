09d5f74d333eccf7d917b4d3c2c0fd26a6498fa0
Manas <qoshboqovmanas@gmail.com>
2026-02-15 02:58:26 +0500

    Add functions to check if a number is even or odd

--- basic/check_number.py
+++ basic/check_number.py
@@ -1,1 +1,11 @@
-
+1.
+def toq_juft(a):
+    if a%2==0:
+        return "Juft"
+    else:
+        return "Toq"
+print(toq_juft(7))
+2.
+def juft_toq(a):
+    return "juft" if a%2==0 else "toq"
+print(juft_toq(8))
