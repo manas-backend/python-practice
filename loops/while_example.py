8e4dd70d01e5e10394fe2c5302aaecff0f90b3b4
Manas <qoshboqovmanas@gmail.com>
2026-02-14 23:54:56 +0000

    +

--- loops/while_example.py
+++ loops/while_example.py
@@ -1,8 +1,20 @@
+# a=int(input("a = "))
+# b=0
+# s=1
+# while a>0:
+#     b=a%10
+#     s*=b
+#     a//=10
+# print(s)
+# 6.
 a=int(input("a = "))
+c=a
 b=0
-s=1
+sum=0
 while a>0:
     b=a%10
-    s*=b
+    sum+=b
     a//=10
-print(s)
+if sum%len(str(c))==0:
+    print(True)
+else: print(False)
