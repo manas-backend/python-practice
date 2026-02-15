22f5a939fe52c0d4484976500913613d51967d89
Manas <qoshboqovmanas@gmail.com>
2026-02-15 02:29:01 +0000

    +

--- loops/while_example.py
+++ loops/while_example.py
@@ -7,14 +7,73 @@
 #     a//=10
 # print(s)
 # 6.
-a=int(input("a = "))
-c=a
-b=0
-sum=0
-while a>0:
-    b=a%10
-    sum+=b
-    a//=10
-if sum%len(str(c))==0:
-    print(True)
-else: print(False)
+# a=int(input("a = "))
+# c=a
+# b=0
+# sum=0
+# while a>0:
+#     b=a%10
+#     sum+=b
+#     a//=10
+# if sum%len(str(c))==0:
+#     print(True)
+# else: print(False)
+# 7.
+# a=int(input("a = "))
+# plus=0
+# minus=0
+# while a!=0:
+#     if a>0:
+#         plus+=1
+#     else:
+#         minus+=1
+#     a=int(input("a = "))
+# print("Musbat sonlar:", plus)
+# print("Manfiy sonlar:", minus)
+# 8.
+# a=int
+# s=0
+# while a!=0:
+#     a=int(input("a = "))
+#     if a%2!=0:
+#         s+=a
+# print("Toq sonlar yig'indisi:", s)
+# 9
+# a=int(input("N = "))
+# b=1
+# c=0
+# i=0
+# while a>i:    
+#     print(c,end=" ")
+#     c,b=b,b+c
+#     i+=1
+# 10.
+# a=int(input("a = "))
+# i=1
+# while i<=a:
+#     print(i*i,end=", ")
+#     i+=1
+# 1.
+# number=1
+# while number<=10:
+#     print(number,end=", ")
+#     number+=1
+# 2.
+# a=int(input("a = "))
+# i=1
+# while i<=10:
+#     print(f"{i}*{a}={i*a}")
+#     i+=1
+# 3.
+
+# i=1
+# while i<=20:
+#     if i%2==0:
+#         print(i,end=", ")
+#     i+=1
+# 4.
+# i=1
+# while i<=20:
+#     if i%2!=0:
+#         print(i,end=", ")
+#     i+=1
