0232d6cd039e1ec90db54e7642785a647af69ca1
Manas <qoshboqovmanas@gmail.com>
2026-02-15 14:53:46 +0000

    Add additional functions for number operations in while_example.py

--- loops/while_example.py
+++ loops/while_example.py
@@ -77,3 +77,87 @@
 #     if i%2!=0:
 #         print(i,end=", ")
 #     i+=1
+# 5.
+# a=int(input("a="))+1
+# b=int(input("b="))
+# sum=0
+# while a<b:
+#     sum+=a
+#     a+=1
+#     print(a)
+# print(sum)
+# 6.
+# a=int(input("a="))+1
+# b=int(input("b="))
+# kopayt=1
+# while a<b:
+#     kopayt*=a
+#     a+=1
+# print(kopayt)
+# 7.
+# a = int(input("a = "))+1
+# b = int(input("b = "))
+# i = a + 1   
+# while a < b:
+#     if a % 2 == 0:
+#         print(a)
+#     a += 1
+# 8.
+# a=int(input("a="))+1
+# b=int(input("b="))
+# while a<b:
+#     if a%2==1:
+#         print(a)
+#     a+=1
+# 9.
+# a=int(input("a="))+1
+# b=int(input("b="))
+# sum=0
+# k=1
+# while a<b:
+#     if a%2==0:
+#         sum+=a
+#         a+=1
+#     else:
+#         k*=a
+#         a+=1
+        
+# print("summa",sum,"\nkopaytma",k)
+10.
+# n = int(input("N=: "))
+# i = 1
+# while i<=n:
+#     if n%i==0:
+#         print(i)
+#     i+=1
+# 11.
+# n=int(input("N= "))
+# i=1
+# k=0
+# sum=0
+# while i<=n:
+#     k+=i
+#     if n%i==0:
+#         sum+=i
+#     i+=1
+# print(k-sum)
+# 12.
+# a=int(input("a="))
+# i=1
+# sum=0
+# while i<a:
+#     if a%i==0:
+#         sum+=i
+#         print(i)
+#     i+=1
+# if sum==a:print("mukammal son")
+# else:print("mukammal emas")
+13.
+a=int(input("a="))
+i=a
+sum=0
+while i>0:
+    b=i%10
+    sum+=b
+    i//=10
+print(sum)
